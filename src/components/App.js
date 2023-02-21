410b9f2d2b4d661dfe0a1ce520277390135bbff4
janeesh <janeesh.tin@gmail.com>
2023-02-22 00:58:29 +0530

    api req handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
+import React, { Component } from "react";
 import Navbar from "./Navbar";
 
-function App() {
-  return (
-    <Navbar />
-      
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      searchTerm: "",
+    }
+    this.apiKey = process.env.React_APP_API;
+  }
+
+  handleSubmit= ()=>{
+    fetch(``)
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
